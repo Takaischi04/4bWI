b0ba4e955414ee8db60215d2e46b915be96566d9
Tamir <tamir@htld>
2023-11-17 14:33:51 +0100

    changes: upcoming matches | state: finished | javaScript, css

--- AJAX/Football/script.js
+++ AJAX/Football/script.js
@@ -1,5 +1,5 @@
 const loadData = () => {
-    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
+    fetch("https://api.openligadb.de/getbltable/öbl1/2023").then((result) => {
         result.json().then((data) => {
             console.log(data);
             fillTable(data);
@@ -8,7 +8,7 @@ const loadData = () => {
 };
 
 const loadDataForContent = (teamID) => {
-    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
+    fetch("https://api.openligadb.de/getbltable/öbl1/2023").then((result) => {
         result.json().then((data) => {
             buttonClicked(data, teamID);
         });
@@ -39,10 +39,14 @@ loadData();
 
 const buttonClicked = (data, teamID) => {
     let html = "";
+    var randomNum1 = Math.floor(Math.random() * 11);
+    var randomNum2 = Math.floor(Math.random() * 11);
 
     if (data[teamID]) {
         const dataElementAtTeamID = data[teamID];
-        const dataElementAtTeamID2 = data[teamID +1];
+        const dataElementAtTeamID2 = data[randomNum1];
+        const dataElementAtTeamID3 = data[randomNum2];
+
         html += "<div class='logoWithName'>";
         html +=     "<img class='imageSize' src=" + dataElementAtTeamID.teamIconUrl + ">";
         html +=     "<div>" + dataElementAtTeamID.teamName + "</div>";
@@ -59,13 +63,23 @@ const buttonClicked = (data, teamID) => {
         html += "</div>";
 
         html += "<div class='nextMatch'>";
-        html +=     "<div class='headerNextMatch'>"+ "Upcoming Matches" + "</div>";
-        html +=         "<div class='nextMatchBox1'>";
-        html +=         "<div class=''>" + dataElementAtTeamID.teamIconUrl + "</div>";
-        html +=         "<div class=''>" + dataElementAtTeamID2.teamIconUrl + "</div>";
+        html += "<h1>Umcoming matches</h1>";
+        html +=     "<div class='nextMatchBox1Back'>";
+        html +=         "<div class='nextMatchBoxFront'>";
+        html +=             "<img class='icon' src=" + dataElementAtTeamID.teamIconUrl + ">";
+        html +=             "<img class='icon' src=" + dataElementAtTeamID2.teamIconUrl + ">";
+        html +=         "</div>";
+        html +=     "</div>";
+        html +=     "<div class='nextMatchBox2Back'>";
+        html +=         "<div class='nextMatchBoxFront'>";
+        html +=             "<img class='icon' src=" + dataElementAtTeamID.teamIconUrl + ">";
+        html +=             "<img class='icon' src=" + dataElementAtTeamID3.teamIconUrl + ">";
+        html +=         "</div>";
         html +=     "</div>";
         html += "</div>";
     }
 
     document.getElementById("content").innerHTML = html;
 };
+
+
