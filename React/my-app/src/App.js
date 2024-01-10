266b9cf4bbc6c6f42558724547a5463484b080c8
Tamir <tamir@htld>
2024-01-10 12:01:36 +0100

    10.01.24|React|Aufgabe1:Liste|Fertig

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,10 +2,11 @@ import List from './components/com_List.js';
 
 function App() {
   return (
-    <div>
-      <List beverage="coffee"/>
-      <List beverage="Tea"/>
-      <List beverage="Beer"/>
+    <div className="m-5">
+      <h1>Liste</h1>
+      <List beverage="coffee" />
+      <List beverage="Tea" />
+      <List beverage="Beer" />
     </div>
   );
 }
--- React/my-app/src/components/com_List.js
+++ React/my-app/src/components/com_List.js
@@ -1,9 +1,8 @@
-import React from 'react'
-
-export default function com_List(beverage) {
+export default function com_List({beverage}) {
+  console.log("beverage: ", beverage);
   return (
     <div>
-        <p>{beverage}</p>
+      <ul className='bg-cyan-600 cursor-pointer w-40 text-xl text-white p-4 mb-1 text-left'>{beverage}</ul>
     </div>
   )
 }
