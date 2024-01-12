aca966d462946a412d0a7debbd089ee994530b79
Tamir <tamir@htld>
2024-01-12 13:40:44 +0100

    12.01.24 React Aufgabe:Button fertig

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -4,13 +4,16 @@ import Button from './components/com_Button.js';
 function App() {
   return (
     <div className="m-5">
-      <h1>Liste</h1>
+      <h1 className="mb-2">Liste</h1>
       <List beverage="coffee" />
       <List beverage="Tea" />
       <List beverage="Beer" />
 
-      <h1 className="mt-5">Button</h1>
+      <h1 className="mt-10 mb-2">Button</h1>
       <Button buttonName="Sendar" />
+
+      <h1 className="mt-10 mb-2">Card</h1>
+
     </div>
   );
 }
--- React/my-app/src/components/com_List.js
+++ React/my-app/src/components/com_List.js
@@ -1,7 +1,5 @@
 export default function com_List({beverage}) {
   return (
-    <div>
-      <ul className='bg-cyan-600 cursor-pointer w-40 text-xl text-white p-4 mb-1 text-left'>{beverage}</ul>
-    </div>
+    <ul className='bg-cyan-600 cursor-pointer w-40 text-xl text-white p-4 mb-1 text-left'>{beverage}</ul>
   )
 }
