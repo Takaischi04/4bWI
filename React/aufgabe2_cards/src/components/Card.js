9800188c9b55c16529fb53ae37575dadb1b9b6dd
Tamir <tamir@htld>
2024-01-19 13:18:17 +0100

    19.01.2024 React aufgabe2_cards fertig

--- React/aufgabe2_cards/src/components/Card.js
+++ React/aufgabe2_cards/src/components/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Card({ name, title, imageUrl }) {
   return (
-    <div>
-      <div>
-        <h2>{name}</h2>
+    <div className="border grid grid-cols-2 h-40">
+      <div className="p-4">
+        <h2 classNAme="font-bold text-xl">{name}</h2>
         <h1>{title}</h1>
       </div>
       <div>
-        <img src={imageUrl} alt=''></img>
+        <img src={imageUrl} alt='' className="object-cover w-full h-full"></img>
       </div>
     </div>
   )
--- React/aufgabe2_cards/src/components/PeopleContainer.js
+++ React/aufgabe2_cards/src/components/PeopleContainer.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card.js';
 
 export default function PeopleContainer() {
+    const [people, setPeople] = useState([]);
+    const [filteredPeople, setFilteredPeople] = useState([]);
+
+    useEffect(() => {
+        fetch("https://65a7a5c594c2c5762da72eb2.mockapi.io/Person").then(
+            (res) => res.json().then((data) => {
+                setPeople(data);
+                setFilteredPeople(data);
+            })
+        );
+    }, [])
+
+    const filterPeople = (filter) => {
+        let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+        setFilteredPeople(filtered);
+    }
+
     return (
-        <div>
-            <h1>People</h1>
-            <div>
-                <Card name="Hans" title="CEO" imageUrl="https://fastly.picsum.photos/id/208/200/300.jpg?hmac=MnC8s7YHdlG81eGO_ZmX1VbjLAYw-jIYEe74kkfvuwc" />
+        <div className="">
+            <div className="fixed mb-4 -full h-28 p-8">
+                <input className="border p-4" type="Text" placeholder="Search" onChange={(el) => {
+                    console.log(el.target.value);
+                    filterPeople(el.target.value);
+                }} />
+            </div>
+
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap p-8 pt-32">
+                {filteredPeople.map(person => {
+                    return <Card name={person.name} imageUrl={person.avatar} title={person.jobtitle} />
+                })}
             </div>
         </div>
     )
