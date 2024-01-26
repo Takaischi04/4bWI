8b00d764ce8b33126f9a7ee9f078a0f15fecda92
Tamir <tamir@htld>
2024-01-26 13:53:03 +0100

    26.01.2024 React learn-router added:Router Pass Data

--- React/learn-router/src/components/Friends.js
+++ React/learn-router/src/components/Friends.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 export default function Friends() {
+    const location = useLocation();
     const navigate = useNavigate();
+
+    console.log(location.state);
     return (
         <div>
             <h1>Friends</h1>
-            <Link to="/">People</Link>
+            <Link to="/Friends" state={{ name: "Hans", age: 17 }}>
+                People
+            </Link>
 
             <div
                 onClick={() => {
