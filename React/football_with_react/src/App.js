fc02de7ead253d13c2def7226eb95be8c75d3a1a
Tamir <tamir@htld>
2024-03-26 23:16:27 +0100

    changed for Previous and Upcoming Matches

--- React/football_with_react/src/App.js
+++ React/football_with_react/src/App.js
@@ -4,15 +4,22 @@ import { useState } from 'react';
 
 function App() {
   const [team, setTeam] = useState(null);
+  const [infoId, setInfoId] = useState(6);
+  const [previousTeam, setPreviousTeam] = useState(null);
+  const [comingTeam, setComingTeam] = useState(null);
 
   return (
     <div className="h-screen w-screen bg-black flex">
-      <List_TeamNameAtom onSelect={(team) => {
+      <List_TeamNameAtom infoId={infoId} onSelect={(team, previousTeam, comingTeam, infoId) => {
         console.log("main", team);
         setTeam(team);
+        setPreviousTeam(previousTeam);
+        setComingTeam(comingTeam);
+        setInfoId(team.teamInfoId);
       }} />
+
       {team ?
-        <Content team={team} /> : <div className="m-10 bg-backgroundContent w-contentWidth font-Quicksand text-white">Bitte wähle ein Team</div>}
+        <Content team={team} previousTeam={previousTeam} comingTeam={comingTeam} /> : <div className="m-10 bg-backgroundContent w-contentWidth font-Quicksand text-white">Bitte wähle ein Team</div>}
     </div>
   );
 }
