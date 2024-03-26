f5bc342f88974ae25293f75f09b716ccb5f99bd7
Tamir <tamir@htld>
2024-03-26 23:14:02 +0100

    included all molecules

--- React/football_with_react/src/components/organisms/Content_Organism.js
+++ React/football_with_react/src/components/organisms/Content_Organism.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import TitleMolecule from '../molecules/Content_TitleMolecule.js'
-{/*
 import StatsMolecule from '../molecules/Content_StatsMolecule.js'
-import ComingMatchesMolecule from '../molecules/Content_ComingMatchesMolecule.js'
-*/}
+import MatchesMolecule from '../molecules/Content_MatchesMolecule.js'
 
-export default function Content_Organism({ team }) {
+export default function Content_Organism({ team, previousTeam, comingTeam }) {
+
   return (
     <div className="m-10 bg-backgroundContent w-contentWidth font-Quicksand text-white">
-      <TitleMolecule team={team} />
-      {/*
-      <StatsMolecule />
-      <ComingMatchesMolecule />
-      */}
+      <div className="flex">
+        <TitleMolecule team={team} />
+        <StatsMolecule team={team} />
+      </div>
+      <MatchesMolecule team={team} previousTeam={previousTeam} comingTeam={comingTeam} />
     </div>
   )
 }
