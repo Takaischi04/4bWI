66c66b5bd4ecfea7f050e94261e19e5cdd911bf0
Tamir <tamir@htld>
2024-03-26 23:13:02 +0100

    Molecule for Previous and Upcoming Matches added

---
+++ React/football_with_react/src/components/molecules/Content_MatchesMolecule.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PreviousMatch from '../atoms/Content_MatchesPreviousAtom'
+import UpcomingMatch from '../atoms/Content_MatchesUpcomingAtom'
+
+export default function Content_ComingMatchesMolecule({ team, previousTeam, comingTeam }) {
+
+  return (
+    <div className="flex pt-10 justify-evenly">
+
+      <div>
+        <h1 className="text-4xl p">Previous Match</h1>
+        <div className="bg-gradient-to-b from-accentTurquoise to-accentPurple p-0.5 h-48 w-96">
+          <div className="bg-backgroundContent h-full">
+            <PreviousMatch team={team} previousTeam={previousTeam} />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-4xl p">Upcoming Matches</h1>
+        <div className="bg-gradient-to-b from-accentTurquoise to-accentPurple p-0.5 h-48 w-96">
+          <div className="bg-backgroundContent h-full">
+            <UpcomingMatch team={team} comingTeam={comingTeam} />
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
